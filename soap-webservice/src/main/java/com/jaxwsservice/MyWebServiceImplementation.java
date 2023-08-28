6dcf46db98b3c0ad87b712083d7a0d931f53ab69
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-28 14:45:28 -0600

    Defining a Web Service

--- soap-webservice/src/main/java/com/jaxwsservice/MyWebServiceImplementation.java
+++ soap-webservice/src/main/java/com/jaxwsservice/MyWebServiceImplementation.java
@@ -1,4 +1,26 @@
 package com.jaxwsservice;
 
+import jakarta.jws.WebMethod; // @interface WebMethod
+import jakarta.jws.WebService; // @interface WebService
+
+@WebService
 public class MyWebServiceImplementation {
+
+    @WebMethod
+    public String myGreetingMessage(String myStr) {
+
+        return "Hello, " + myStr + ". Welcome to the world of Web Services!";
+    }
 }
+
+// Older Tomcat users (versions 9 or older)
+// Use version 2.3.2 for jaxws-rt dependency
+
+// Older Tomcat users
+// Use javax.jws.WebMethod and javax.jws.WebService;
+
+// In order to solve Exception in thread "main" java.lang.NoClassDefFoundError: jakarta/activation/DataSource
+// https://mvnrepository.com/artifact/jakarta.activation/jakarta.activation-api/2.0.0
+
+// Older Tomcat users
+// Head to https://mvnrepository.com/artifact/javax.activation/javax.activation-api/1.2.0
---
+++ soap-webservice/src/main/java/com/jaxwsservice/MyWebServicePublisher.java
@@ -0,0 +1,17 @@
+package com.jaxwsservice;
+
+import jakarta.xml.ws.Endpoint; // abstract class Endpoint
+
+public class MyWebServicePublisher {
+
+    public static void main(String[] args) {
+        //WARNING: An illegal reflective access operation has occurred
+        //WARNING: Illegal reflective access by com.sun.xml.ws.model.Injector (file:/Users/juan/.m2/repository/com/sun/xml/ws/jaxws-rt/3.0.0/jaxws-rt-3.0.0.jar) to method java.lang.ClassLoader.defineClass(java.lang.String,byte[],int,int)
+        //WARNING: Please consider reporting this to the maintainers of com.sun.xml.ws.model.Injector
+        //WARNING: Use --illegal-access=warn to enable warnings of further illegal reflective access operations
+        //WARNING: All illegal access operations will be denied in a future release
+
+        Endpoint.publish("http://localhost:8080/webservice/greeting",
+                new MyWebServiceImplementation());
+    }
+}
